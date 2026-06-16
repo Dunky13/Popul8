a70560ab848a580ac8ccbbf7428a93a8c3301f3c
Marc Went <marc@went.io>
2026-06-16 10:25:51 +0200

    feat: route DM vs player by room query param

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { DmScreen } from './ui/DmScreen';
+import { PlayerScreen } from './ui/PlayerScreen';
+
 export function App() {
-  return <h1>Initiative Tracker</h1>;
+  const room = new URLSearchParams(window.location.search).get('room');
+  return room ? <PlayerScreen roomCode={room} /> : <DmScreen />;
 }
